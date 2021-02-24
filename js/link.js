72989d67535a90412c5beb33c19c55d7985e6a7a
praveenanandh <64463469+praveenanandh@users.noreply.github.com>
2021-02-24 22:16:11 +0530

    first version

---
+++ js/link.js
@@ -0,0 +1,12 @@
+//.HTML files link
+
+
+//Light theme
+function light() {
+    window.open("light.html" ,"_self");
+    }
+
+//Dark theme
+function dark() {
+    window.open("dark.html","_self");
+    }
---
+++ js/navbar.js
@@ -0,0 +1,10 @@
+	// scrolls down navbar
+		window.onscroll = function() {scrollFunction()};
+
+		function scrollFunction() {
+		  if (document.body.scrollTop > 0 || document.documentElement.scrollTop > 0) {
+			document.getElementById("navbar").style.top = "0";
+		  } else {
+			document.getElementById("navbar").style.top = "-110px";
+		  }
+		}
